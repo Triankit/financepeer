38b8a238c61f661ee1b8d427fcf1893f1c228d52
Triankit <tripathi.ankit83@gmail.com>
2022-09-01 10:15:52 +0530

    task2&3complete

--- src/components/screens/homeScreen.js
+++ src/components/screens/homeScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import {View, Text, ImageBackground, StyleSheet, FlatList} from 'react-native';
+import {Card, Paragraph} from 'react-native-paper';
+import {BackgroundImage} from '../features/backgroundImages';
 export const HomeScreen = () => {
+  const ImageData = [
+    require('../../assests/image1.jpg'),
+    require('../../assests/image2.jpg'),
+    require('../../assests/image3.jpg'),
+    require('../../assests/image4.jpg'),
+    require('../../assests/image5.jpg'),
+  ];
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <BackgroundImage>
+      <FlatList
+        data={ImageData}
+        keyExtractor={(_, index) => index}
+        renderItem={({item}) => {
+          console.log(item);
+          return (
+            <Card style={styles.cover}>
+              <Card.Content>
+                <Card.Cover source={item} />
+              </Card.Content>
+            </Card>
+          );
+        }}
+      />
+    </BackgroundImage>
   );
 };
+
+const styles = StyleSheet.create({
+  cover: {
+    margin: 16,
+  },
+});
