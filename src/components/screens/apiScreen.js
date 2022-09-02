c87961197e73b08a2d2ee9543837a8a2fe1c74f4
Triankit <tripathi.ankit83@gmail.com>
2022-09-02 19:59:21 +0530

    update activity indicator

--- src/components/screens/apiScreen.js
+++ src/components/screens/apiScreen.js
@@ -1,29 +1,45 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Axios from 'axios';
+import {ActivityIndicator} from 'react-native-paper';
 
 export const ApiScreen = () => {
   const [items, setItem] = useState([]);
+  const [visibilty, setVisibility] = useState(true);
   useEffect(() => {
     (async () => {
       const response = await Axios.get(
         'https://jsonplaceholder.typicode.com/posts',
       );
       setItem(response.data);
+      setVisibility(false);
     })();
   }, []);
   return (
-    <FlatList
-      data={items}
-      renderItem={({item}) => (
-        <View style={styles.item}>
-          <Text style={styles.id}>userId : {item.userId}</Text>
-          <Text style={styles.id}>id : {item.id}</Text>
-          <Text style={styles.title}>title : {item.title}</Text>
-          <Text style={styles.title}>body : {item.body}</Text>
-        </View>
+    <React.Fragment>
+      {visibilty && (
+        <ActivityIndicator
+          animating={visibilty}
+          size="large"
+          style={styles.indicator}
+          color="#826CD4"
+          hidesWhenStopped={!visibilty}
+        />
       )}
-    />
+      {!visibilty && (
+        <FlatList
+          data={items}
+          renderItem={({item}) => (
+            <View style={styles.item}>
+              <Text style={styles.id}>userId : {item.userId}</Text>
+              <Text style={styles.id}>id : {item.id}</Text>
+              <Text style={styles.title}>title : {item.title}</Text>
+              <Text style={styles.title}>body : {item.body}</Text>
+            </View>
+          )}
+        />
+      )}
+    </React.Fragment>
   );
 };
 
@@ -47,5 +63,8 @@ const styles = StyleSheet.create({
   id: {
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  indicator: {
+    margin: '50%',
   },
 });
